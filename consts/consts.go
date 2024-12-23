6041e2fd9b6159ea8d87dfc1d66822a8574cee79
Sravancsv45 <ajaykr3367@gmail.com>
2024-12-23 20:09:55 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "SRV"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "SRVN"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "SRAVN369"
 )
 
 var ID ids.ID
